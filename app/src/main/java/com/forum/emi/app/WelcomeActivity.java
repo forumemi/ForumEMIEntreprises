286b3bcbf850b29af51f942579d1c53cb17aa37d
salah <salah.elhssani@gmail.com>
2018-12-22 11:00:59 +0100

    welcomeButton redirect to MainActivity

--- app/src/main/java/com/forum/emi/app/WelcomeActivity.java
+++ app/src/main/java/com/forum/emi/app/WelcomeActivity.java
@@ -1,13 +1,29 @@
 package com.forum.emi.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button welcomeButton = null;
+
+    View.OnClickListener welcomeButtonListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        welcomeButton = (Button)findViewById(R.id.welcomeButton);
+        welcomeButton.setOnClickListener(welcomeButtonListner);
     }
 }
