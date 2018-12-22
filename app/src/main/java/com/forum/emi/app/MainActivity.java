084d70a6286962d9756204982f2de8724f87e2c9
salah <salah.elhssani@gmail.com>
2018-12-22 15:14:09 +0100

    Edit Navigation drawer

--- app/src/main/java/com/forum/emi/app/MainActivity.java
+++ app/src/main/java/com/forum/emi/app/MainActivity.java
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_program) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_plan) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_companies) {
 
         } else if (id == R.id.nav_share) {
 
