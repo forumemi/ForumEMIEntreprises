65a4c25f018345efc2e0c312f622440092b71122
salah <salah.elhssani@gmail.com>
2018-12-23 15:47:34 +0100

    Functional signing in and signing out

--- app/src/main/java/com/forum/emi/app/MainActivity.java
+++ app/src/main/java/com/forum/emi/app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.forum.emi.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -14,16 +16,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener  {
+
+    private static final int RC_SIGN_IN = 123;
+    private SharedPreferences sharedPreferences = null;
+    MenuItem actionSignin = null;
+    MenuItem actionSignout = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +62,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
     }
 
     @Override
@@ -62,8 +76,25 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+        MenuItem actionSignin = menu.findItem(R.id.action_signin);
+        MenuItem actionSignout = menu.findItem(R.id.action_signout);
+        String userid = sharedPreferences.getString("USER_ID","");
+        if (userid == "") {
+            actionSignin.setVisible(true);
+            actionSignout.setVisible(false);
+        } else {
+            actionSignin.setVisible(false);
+            actionSignout.setVisible(true);
+        }
+        return true;
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -81,10 +112,60 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_signin)  {
+            createSignInIntent();
+        } else if (id == R.id.action_signout)  {
+            FirebaseAuth.getInstance().signOut();
+            SharedPreferences.Editor editor =sharedPreferences.edit();
+            editor.putString("USER_ID","");
+            editor.commit();
+            Intent intent = new Intent(MainActivity.this,MainActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+        // [END auth_fui_create_intent]
+    }
+
+    // [START auth_fui_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("USER_ID",user.getUid());
+                editor.commit();
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+    // [END auth_fui_result]
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -140,6 +221,9 @@ public class MainActivity extends AppCompatActivity
             ConstraintLayout companiesLayout = (ConstraintLayout)findViewById(R.id.companies_layout);
             companiesLayout.setVisibility(View.VISIBLE);
             //end
+
+            String userId = sharedPreferences.getString("USER_ID","");
+            Toast.makeText(MainActivity.this,userId,Toast.LENGTH_SHORT).show();
 
         } else if (id == R.id.nav_qrcode) {
             Intent intent = new Intent(MainActivity.this,ScannerActivity.class);
